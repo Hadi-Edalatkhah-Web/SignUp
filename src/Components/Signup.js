0dca5788d727672d12caa870a08a089263a10ae1
Hadi Edalatkhah <106934932+Hadi-Edalatkhah-Web@users.noreply.github.com>
2022-07-15 17:03:22 +0430

    Add files via upload

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,8 +1,11 @@
 import React,{useState, useEffect} from 'react';
 
+//Function
+import { Validate } from "./Validate.js";
 const Signup = () => {
 
-    // States
+    /////////////////////////////////// States /////////////////////////////
+
     const [data,setData]=useState({
         name:"",
         email:"",
@@ -10,9 +13,66 @@ const Signup = () => {
         confirmPassword:"",
         isAccepted:false,
     })
+    const [errors,setErrors]=useState({})
+
+    ///////////////////////////// Validation /////////////////////////////
+
+   useEffect(()=>{
+       setErrors(Validate(data))
+       console.log(data)
+   },[data])
+
+
+    ///////////////////////////// Functions /////////////////////////////
+
+    const changeHandler=(e)=>{
+     if(e.target.name === "isAccepted")
+     {
+        setData({
+            ...data, 
+            [e.target.name]:e.target.checked
+        })
+
+     }
+     else{
+        setData({
+            ...data,
+            [e.target.name]:e.target.value
+        })
+     }
+    }
+    
+   
+    
     return (
         <div>
-            Hadi
+          
+            <div>
+                <span>Name</span>
+                <input name='name' placeholder='UserName'  type="text" onChange={changeHandler} value={data.name}/>
+              
+            </div>
+            <div>
+                <span>Email</span>
+                <input name='email' placeholder='Email'  type="email"  onChange={changeHandler} value={data.email}/>
+            </div>
+            <div>
+                <span>Password</span>
+                <input name='password' placeholder='Password'  type="password"  onChange={changeHandler} value={data.password}/>
+            </div>
+             <div>
+                <span>confirmPassword</span>
+                <input name='confirmPassword' placeholder='ConfirmPassword'  type="text"  onChange={changeHandler} value={data.confirmPassword}/>
+            </div>
+            <div>
+                <span>I accepted terms of privacy policy</span>
+                <input name='isAccepted'  type="checkbox"  onChange={changeHandler} value={data.isAccepted}/>
+            </div>
+           <div>
+            <a>SignIn</a>
+            <button>SignUp</button>
+           </div>
+           
             
         </div>
     );
--- src/Components/Validate.js
+++ src/Components/Validate.js
@@ -1,4 +1,4 @@
- const validate=(data)=>{
+export const Validate=(data)=>{
     const errors={};
 
     // Name ++++++++++++++++++++++++++++
@@ -13,5 +13,59 @@
     }
 
 
-    // 
+    //  Email +++++++++++++++++++++++++++++
+    ///////////////////////////////////////
+
+    const emailRegex= /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; 
+    if(!data.email.trim())
+    {
+        errors.email="name required"
+
+    }
+    else if(!emailRegex.test(data.email))
+    {
+        errors.email="email invalid"
+    }
+    else{
+        delete errors.email;
+    }
+
+
+    // Password +++++++++++++++++++++++++++
+    ///////////////////////////////////////
+
+    const passwordRegex=/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
+    if(!passwordRegex.test(data.password))
+    {
+        errors.email="use UpperCase ande LowerCase and $%^& in your password"
+
+    }
+    else{
+        delete errors.email;
+    }
+
+    // confirmPassword ++++++++++++++++++++
+    ///////////////////////////////////////
+
+    if(!data.confirmPassword === data.password)
+    {
+        errors.confirmPassword="passwords not match"
+    }
+    else
+    {
+        delete errors.confirmPassword
+    }
+    // Privacy Policy +++++++++++++++++++++
+    ///////////////////////////////////////
+
+    if(data.isAccepted === false)
+    {
+        errors.isAccepted="please accept our realations"
+    }
+    else
+    {
+        delete errors.isAccepted
+    }
+
  }
+
